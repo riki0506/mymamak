f928c39ed0c0c2a2a641d9279ddffd74facea936
riki0506 <rikinakajima0506@gmail.com>
2023-12-30 11:25:34 +0900

    init_relations_setup

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    
+    public function country()
+{
+    return $this->belongsTo(Country::class);
+}
+
+    public function dish()
+{
+    return $this->belongsTo(Dish::class);
+}
+
+    public function restaurant()
+{
+    return $this->belongsTo(Restaurant::class);
+}
+
 }
