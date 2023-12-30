67ee2806cf4a0d960cac25a8ba00d47bfa398ff8
riki0506 <rikinakajima0506@gmail.com>
2023-12-30 10:20:22 +0900

    add_userid_column_poststable

--- database/migrations/2023_12_29_133028_create_likes_table.php
+++ database/migrations/2023_12_29_133028_create_likes_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
             $table->timestamps();
         });
     }
