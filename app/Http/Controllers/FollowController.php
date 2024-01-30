364ad2218224fe54bcd2120bf57b1300773d3710
riki0506 <rikinakajima0506@gmail.com>
2024-01-30 18:25:54 +0900

    debug user index view

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -9,18 +9,18 @@ use App\Models\User;
 class FollowController extends Controller
 {
 
-    public function follow(User $user)
-    {
-        auth()->user()->following()->attach($user);
+    // public function follow(User $user)
+    // {
+    //     auth()->user()->following()->attach($user);
     
-        return back()->with('success', 'You are now following ' . $user->name);
-    }
+    //     return back()->with('success', 'You are now following ' . $user->name);
+    // }
     
-    public function unfollow(User $user)
-    {
-        auth()->user()->following()->detach($user);
+    // public function unfollow(User $user)
+    // {
+    //     auth()->user()->following()->detach($user);
     
-        return back()->with('success', 'You have unfollowed ' . $user->name);
-    }
+    //     return back()->with('success', 'You have unfollowed ' . $user->name);
+    // }
 
 }
